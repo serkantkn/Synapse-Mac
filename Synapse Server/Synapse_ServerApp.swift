52cecba7547eb675b2e9f704fe7bfd266272949b
serkantkn <serkantkn5@gmail.com>
2026-04-15 22:09:36 +0300

    Initial Commit

---
+++ Synapse Server/Synapse_ServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Synapse_ServerApp.swift
+//  Synapse Server
+//
+//  Created by Serkan on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Synapse_ServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
